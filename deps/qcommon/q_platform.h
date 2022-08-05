016e2f1564d12c59ec259b4d3ee58da7edc9a0da
Martin Gerhardy <martin.gerhardy@gmail.com>
2022-08-05 19:56:19 +0200

    QCOMMON: removed ARCH_STRING and PLATFORM_STRING

--- deps/qcommon/q_platform.h
+++ deps/qcommon/q_platform.h
@@ -23,16 +23,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef __Q_PLATFORM_H
 #define __Q_PLATFORM_H
 
-// this is for determining if we have an asm version of a C function
-#ifdef Q3_VM
-
-#define id386 0
-#define idppc 0
-#define idppc_altivec 0
-#define idsparc 0
-
-#else
-
 #if (defined _M_IX86 || defined __i386__) && !defined(C_ONLY)
 #define id386 1
 #else
@@ -92,12 +82,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE inline
 #define PATH_SEP '\\'
 
-#if defined(__WIN64__)
-#define ARCH_STRING "x86_64"
-#elif defined _M_ALPHA
-#define ARCH_STRING "AXP"
-#endif
-
 #define Q3_LITTLE_ENDIAN
 
 #define DLL_EXT ".dll"
@@ -119,12 +103,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE __inline
 #define PATH_SEP '\\'
 
-#if defined(_M_IX86) || defined(__i386__)
-#define ARCH_STRING "x86"
-#elif defined _M_ALPHA
-#define ARCH_STRING "AXP"
-#endif
-
 #define Q3_LITTLE_ENDIAN
 
 #define DLL_EXT ".dll"
@@ -145,13 +123,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define PATH_SEP '/'
 
 #ifdef __ppc__
-#define ARCH_STRING "ppc"
 #define Q3_BIG_ENDIAN
 #elif defined __i386__
-#define ARCH_STRING "i386"
 #define Q3_LITTLE_ENDIAN
 #elif defined __x86_64__
-#define ARCH_STRING "x86_64"
 #define Q3_LITTLE_ENDIAN
 #endif
 
@@ -168,36 +143,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define OS_STRING "linux"
 #define ID_INLINE inline
 #define PATH_SEP '/'
-
-#if defined __i386__
-#define ARCH_STRING "i386"
-#elif defined __x86_64__
-#define ARCH_STRING "x86_64"
-#elif defined __powerpc64__
-#define ARCH_STRING "ppc64"
-#elif defined __powerpc__
-#define ARCH_STRING "ppc"
-#elif defined __s390__
-#define ARCH_STRING "s390"
-#elif defined __s390x__
-#define ARCH_STRING "s390x"
-#elif defined __ia64__
-#define ARCH_STRING "ia64"
-#elif defined __alpha__
-#define ARCH_STRING "alpha"
-#elif defined __sparc__
-#define ARCH_STRING "sparc"
-#elif defined __arm__
-#define ARCH_STRING "arm"
-#elif defined __cris__
-#define ARCH_STRING "cris"
-#elif defined __hppa__
-#define ARCH_STRING "hppa"
-#elif defined __mips__
-#define ARCH_STRING "mips"
-#elif defined __sh__
-#define ARCH_STRING "sh"
-#endif
 
 #if __FLOAT_WORD_ORDER == __BIG_ENDIAN
 #define Q3_BIG_ENDIAN
@@ -231,14 +176,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE inline
 #define PATH_SEP '/'
 
-#ifdef __i386__
-#define ARCH_STRING "i386"
-#elif defined __amd64__
-#define ARCH_STRING "amd64"
-#elif defined __axp__
-#define ARCH_STRING "alpha"
-#endif
-
 #if BYTE_ORDER == BIG_ENDIAN
 #define Q3_BIG_ENDIAN
 #else
@@ -260,12 +197,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE inline
 #define PATH_SEP '/'
 
-#ifdef __i386__
-#define ARCH_STRING "i386"
-#elif defined __sparc
-#define ARCH_STRING "sparc"
-#endif
-
 #if defined(_BIG_ENDIAN)
 #define Q3_BIG_ENDIAN
 #elif defined(_LITTLE_ENDIAN)
@@ -284,37 +215,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE __inline
 #define PATH_SEP '/'
 
-#define ARCH_STRING "mips"
-
 #define Q3_BIG_ENDIAN // SGI's MIPS are always big endian
 
 #define DLL_EXT ".so"
 
 #endif
 
-//================================================================== Q3VM ===
-
-#ifdef Q3_VM
-
-#define OS_STRING "q3vm"
-#define ID_INLINE
-#define PATH_SEP '/'
-
-#define ARCH_STRING "bytecode"
-
-#define DLL_EXT ".qvm"
-
-#endif
-
-//===========================================================================
-
 // catch missing defines in above blocks
 #if !defined(OS_STRING)
 #error "Operating system not supported"
-#endif
-
-#if !defined(ARCH_STRING)
-#error "Architecture not supported"
 #endif
 
 #ifndef ID_INLINE
@@ -353,26 +262,6 @@ float FloatSwap(const float *f);
 #define BigShort(x) ShortSwap(x)
 #define BigLong(x) LongSwap(x)
 #define BigFloat(x) FloatSwap(&x)
-
-#elif defined(Q3_VM)
-
-#define LittleShort
-#define LittleLong
-#define LittleFloat
-#define BigShort
-#define BigLong
-#define BigFloat
-
-#else
-#error "Endianness not defined"
-#endif
-
-// platform string
-#ifdef NDEBUG
-#define PLATFORM_STRING OS_STRING "-" ARCH_STRING
-#else
-#define PLATFORM_STRING OS_STRING "-" ARCH_STRING "-debug"
-#endif
 
 #endif
 
